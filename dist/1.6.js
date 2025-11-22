ace7f09e2358c460a4b50ec0c3590e85f57f9c83
halder25572 <halder25572.com>
2025-11-22 23:04:07 +0600

    function method learn

--- dist/1.6.js
+++ dist/1.6.js
@@ -4,8 +4,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 function add(num1, num2) {
     return num1 + num2;
 }
-console.log(add(20, 30));
+// console.log(add(20, 30));
 // arrow function ts
+// arrow function
 const add2 = (num1, num2) => num1 + num2;
-console.log(add2(10, 20));
+const sum1 = add2(20, 30);
+// console.log(sum1);
+// object --> function --> method
+const poorUser = {
+    name: "Nayan",
+    balance: 0,
+    addBalance(balance) {
+        return `My new balance is ${this.balance + balance}`;
+    }
+};
+const poorBalance = poorUser.addBalance(200);
+console.log(poorBalance);
 //# sourceMappingURL=1.6.js.map
--- src/1.6.ts
+++ src/1.6.ts
@@ -6,5 +6,19 @@ function add(num1: number, num2: number){
 console.log(add(20, 30));
 // arrow function ts
 
-const add2 = (num1: number, num2: number) => num1 + num2;
-console.log(add2(10, 20));
+// arrow function
+const add2 = (num1: number, num2: number): number => num1 + num2;
+const sum1 = add2(20,30);
+console.log(sum1);
+
+// object --> function --> method
+const poorUser = {
+    name: "Nayan",
+    balance: 0,
+    addBalance(balance:number): string{
+        return `My new balance is ${this.balance + balance}`;
+    }
+}
+
+const poorBalance = poorUser.addBalance(200);
+console.log(poorBalance);
